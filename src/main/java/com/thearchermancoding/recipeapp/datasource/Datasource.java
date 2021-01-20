515652b5fa863cf78c8e8e0b5ff85768ac61a78f
Owen Hayes <owen.v.hayes@gmail.com>
2021-01-20 10:22:18 -0800

    Added separate dev and prod environments. - dev environment uses H2. - prod environment uses Postgres.

--- src/main/java/com/thearchermancoding/recipeapp/datasource/Datasource.java
+++ src/main/java/com/thearchermancoding/recipeapp/datasource/Datasource.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class PostgresDatasource {
+public class Datasource {
 
     @Bean
     @ConfigurationProperties("app.datasource")
