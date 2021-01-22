f568c4044a92d9cb83bd96e0111218ea25761379
Owen Hayes <owen.v.hayes@gmail.com>
2021-01-21 22:33:17 -0800

    :recycle: Refactored RecipeService
    Removed RecipeService interface. Added the ability to find the average rating by id for a recipe.

--- src/main/java/com/thearchermancoding/recipeapp/services/RecipeService.java
+++ src/main/java/com/thearchermancoding/recipeapp/services/RecipeService.java
@@ -1,5 +1,6 @@
 package com.thearchermancoding.recipeapp.services;
 
+import com.thearchermancoding.recipeapp.models.Rating;
 import com.thearchermancoding.recipeapp.models.Recipe;
 import com.thearchermancoding.recipeapp.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +8,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalDouble;
 
 @Service
-public class RecipeService implements IRecipeService{
+public class RecipeService {
 
     private RecipeRepository repository;
 
@@ -18,35 +20,39 @@ public class RecipeService implements IRecipeService{
         this.repository = repository;
     }
 
-    @Override
     public Iterable<Recipe> getAllRecipes() {
         return repository.findAll();
     }
 
-    @Override
     public Optional<Recipe> getRecipeById(Long id) {
         return repository.findById(id);
     }
 
-    @Override
     public void updateRecipe(Recipe recipe) {
         repository.save(recipe);
     }
 
-    @Override
     public Recipe createRecipe(Recipe recipe) {
         return repository.save(recipe);
     }
 
-    @Override
     public void deleteRecipeById(Long id) {
         repository.deleteById(id);
     }
 
-    @Override
     public void deleteRecipe(Recipe recipe) {
         repository.delete(recipe);
     }
 
-
+    public Optional<Float> getAverageRatingById(Long id) {
+        OptionalDouble average;
+        Optional<Recipe> rating = repository.findById(id);
+        if (rating.isPresent()) {
+            average = rating.get().getRatings().stream().mapToDouble(Rating::getRating).average();
+            if (average.isPresent()) {
+                return Optional.of((float) average.getAsDouble());
+            }
+        }
+        return Optional.empty();
+    }
 }
